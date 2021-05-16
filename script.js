26cecb381886dbb653857a9fc607018e34b91f82
flavlaz <flaviu_lazar@gmail.com>
2021-05-16 21:44:08 +0300

    style changes

--- script.js
+++ script.js
@@ -26,10 +26,10 @@ function move(e){
 }
 
 /*===== GSAP ANIMATION =====*/
-gsap.from('.nav__logo, .nav__toggle', {opacity:0, duraction:1, delay:2, y:10})
-gsap.from('.nav__item', {opacity:0, duraction:1, delay:2.1, y:30, stagger:0.2})
+gsap.from('.nav__logo, .nav__toggle', {opacity:0, duration:1, delay:2, y:10})
+gsap.from('.nav__item', {opacity:0, duration:1, delay:2.1, y:30, stagger:0.2})
 
-gsap.from('.hero__title', {opacity:0, duraction:1, delay:1.6, y:30})
-gsap.from('.hero__desc', {opacity:0, duraction:1, delay:1.8, y:30})
-gsap.from('.hero__btn', {opacity:0, duraction:1, delay:2.1, y:30})
-gsap.from('.hero__img', {opacity:0, duraction:1, delay:1.3, y:30})
+gsap.from('.hero__title', {opacity:0, duration:1, delay:1.6, y:30})
+gsap.from('.hero__desc', {opacity:0, duration:1, delay:1.8, y:30})
+gsap.from('.hero__btn', {opacity:0, duration:1, delay:2.1, y:30})
+gsap.from('.hero__img', {opacity:0, duration:1, delay:1.3, y:30})
